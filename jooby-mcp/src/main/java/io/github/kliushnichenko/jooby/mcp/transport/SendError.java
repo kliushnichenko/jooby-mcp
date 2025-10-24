14b4ac3e260dc1fc8b562f2fa5f09113334e171c
Volodymyr Kliushnichenko <v.kliushnychenko@gmail.com>
2025-10-24 15:53:01 +0300

    improve transport err handling

--- jooby-mcp/src/main/java/io/github/kliushnichenko/jooby/mcp/transport/SendError.java
+++ jooby-mcp/src/main/java/io/github/kliushnichenko/jooby/mcp/transport/SendError.java
@@ -8,97 +8,110 @@ import io.modelcontextprotocol.spec.McpSchema;
 
 import java.util.List;
 
-class Error {
+class SendError {
 
-    static McpSchema.JSONRPCResponse serverIsShuttingDown(Context ctx) {
+    static Context serverIsShuttingDown(Context ctx) {
         ctx.setResponseCode(StatusCode.SERVICE_UNAVAILABLE);
-        return err(
+        var err = err(
                 new McpSchema.JSONRPCResponse.JSONRPCError(
                         McpSchema.ErrorCodes.INTERNAL_ERROR,
                         "Server is shutting down",
                         null)
         );
+        return send(ctx, err);
     }
 
-    static McpSchema.JSONRPCResponse invalidAcceptHeader(Context ctx, List<MediaType> acceptedTypes) {
+    static Context invalidAcceptHeader(Context ctx, List<MediaType> acceptedTypes) {
         ctx.setResponseCode(StatusCode.BAD_REQUEST);
-        return err(
+        var err = err(
                 new McpSchema.JSONRPCResponse.JSONRPCError(
                         McpSchema.ErrorCodes.INVALID_REQUEST,
                         "Invalid Accept header. Expected: %s".formatted(acceptedTypes),
                         null)
         );
+        return send(ctx, err);
     }
 
-    static McpSchema.JSONRPCResponse missingSessionId(Context ctx) {
+    static Context missingSessionId(Context ctx) {
         ctx.setResponseCode(StatusCode.BAD_REQUEST);
-        return err(
+        var err = err(
                 new McpSchema.JSONRPCResponse.JSONRPCError(
                         McpSchema.ErrorCodes.INVALID_REQUEST,
                         "Session ID required in %s header".formatted(HttpHeaders.MCP_SESSION_ID),
                         null)
         );
+        return send(ctx, err);
     }
 
-    static McpSchema.JSONRPCResponse sessionNotFound(Context ctx, String sessionId) {
+    static Context sessionNotFound(Context ctx, String sessionId) {
         ctx.setResponseCode(StatusCode.NOT_FOUND);
-        return err(
+        var err = err(
                 new McpSchema.JSONRPCResponse.JSONRPCError(
                         McpSchema.ErrorCodes.INVALID_REQUEST,
                         "Session %s not found".formatted(sessionId),
                         null)
         );
+        return send(ctx, err);
     }
 
-    static McpSchema.JSONRPCResponse unknownMsgType(Context ctx, String sessionId) {
+    static Context unknownMsgType(Context ctx, String sessionId) {
         ctx.setResponseCode(StatusCode.BAD_REQUEST);
-        return err(
+        var err = err(
                 new McpSchema.JSONRPCResponse.JSONRPCError(
                         McpSchema.ErrorCodes.INVALID_REQUEST,
                         "Unknown message type. Session ID: %s".formatted(sessionId),
                         null)
         );
+        return send(ctx, err);
     }
 
-    static McpSchema.JSONRPCResponse msgParseError(Context ctx, String sessionId) {
+    static Context msgParseError(Context ctx, String sessionId) {
         ctx.setResponseCode(StatusCode.BAD_REQUEST);
-        return err(
+        var err = err(
                 new McpSchema.JSONRPCResponse.JSONRPCError(
                         McpSchema.ErrorCodes.PARSE_ERROR,
                         "Invalid message format. Session ID: %s".formatted(sessionId),
                         null)
         );
+        return send(ctx, err);
     }
 
-    static McpSchema.JSONRPCResponse deletionNotAllowed(Context ctx) {
+    static Context deletionNotAllowed(Context ctx) {
         ctx.setResponseCode(StatusCode.METHOD_NOT_ALLOWED);
-        return err(
+        var err = err(
                 new McpSchema.JSONRPCResponse.JSONRPCError(
                         McpSchema.ErrorCodes.INVALID_REQUEST,
                         "Session deletion is not allowed",
                         null)
         );
+        return send(ctx, err);
     }
 
-    static McpSchema.JSONRPCResponse internalError(Context ctx, String sessionId) {
+    static Context internalError(Context ctx, String sessionId) {
         ctx.setResponseCode(StatusCode.SERVER_ERROR);
-        return err(
+        var err = err(
                 new McpSchema.JSONRPCResponse.JSONRPCError(
                         McpSchema.ErrorCodes.INTERNAL_ERROR,
                         "Internal Server Error. Session ID: %s".formatted(sessionId),
                         null)
         );
+        return send(ctx, err);
     }
 
 
-    public static McpSchema.JSONRPCResponse error(Context ctx, StatusCode statusCode, int errCode, String msg) {
+    public static Context error(Context ctx, StatusCode statusCode, int errCode, String msg) {
         ctx.setResponseCode(statusCode);
-        return err(
+        var err = err(
                 new McpSchema.JSONRPCResponse.JSONRPCError(errCode, msg, null)
         );
+        return send(ctx, err);
     }
 
     private static McpSchema.JSONRPCResponse err(McpSchema.JSONRPCResponse.JSONRPCError err) {
         return new McpSchema.JSONRPCResponse(McpSchema.JSONRPC_VERSION, null, null, err);
+    }
+
+    private static Context send(Context ctx, McpSchema.JSONRPCResponse err) {
+        return ctx.accept(MediaType.json) ? ctx.render(err) : ctx.send(err.toString());
     }
 }
---
+++ jooby-mcp/src/test/java/test/JoobyStreamableTransportTest.java
@@ -0,0 +1,113 @@
+package test;
+
+import io.jooby.StatusCode;
+import io.jooby.test.JoobyTest;
+import io.modelcontextprotocol.spec.HttpHeaders;
+import io.modelcontextprotocol.spec.McpSchema;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static io.modelcontextprotocol.spec.McpSchema.JSONRPC_VERSION;
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@JoobyTest(value = app.StreamableTransportApp.class, port = 8099)
+class JoobyStreamableTransportTest {
+
+    static {
+        RestAssured.port = 8099;
+    }
+
+    private static final McpSchema.JSONRPCRequest DUMMY_BODY = new McpSchema.JSONRPCRequest(
+            JSONRPC_VERSION, "test", "1", null);
+
+    @Nested
+    class Get {
+
+        @Test
+        public void nonEventStream_shouldThrowError() {
+            var result = given()
+                    .header("Accept", "application/xml")
+                    .get("/mcp")
+                    .then()
+                    .assertThat()
+                    .statusCode(StatusCode.BAD_REQUEST_CODE)
+                    .extract().asString();
+
+            assertThat(result).contains("Invalid Accept header. Expected: [text/event-stream]");
+        }
+
+        @Test
+        public void missingSessionId_shouldThrowError() {
+            var result = given().header("Accept", "text/event-stream")
+                    .when()
+                    .get("/mcp")
+                    .then()
+                    .assertThat()
+                    .statusCode(StatusCode.BAD_REQUEST_CODE)
+                    .extract().asString();
+
+            assertThat(result).contains("Session ID required in Mcp-Session-Id header");
+        }
+
+        @Test
+        public void nonExistingSessionId_shouldThrowError() {
+            var result = given()
+                    .header(HttpHeaders.MCP_SESSION_ID, "12345")
+                    .get("/mcp")
+                    .then()
+                    .assertThat()
+                    .statusCode(StatusCode.NOT_FOUND_CODE)
+                    .extract().asString();
+
+            assertThat(result).contains("Session 12345 not found");
+        }
+    }
+
+    @Nested
+    class Post {
+
+        @Test
+        public void illegalAcceptHeader_shouldThrowError() {
+            var result = given()
+                    .header("Accept", "application/xml")
+                    .body(DUMMY_BODY)
+                    .post("/mcp")
+                    .then()
+                    .assertThat()
+                    .statusCode(StatusCode.BAD_REQUEST_CODE)
+                    .extract().asString();
+
+            assertThat(result).contains("Invalid Accept header. Expected: [text/event-stream, application/json]");
+        }
+
+        @Test
+        public void missingSessionId_shouldThrowError() {
+            var result = given().header("Accept", "text/event-stream, application/json")
+                    .body(DUMMY_BODY)
+                    .when()
+                    .post("/mcp")
+                    .then()
+                    .assertThat()
+                    .statusCode(StatusCode.BAD_REQUEST_CODE)
+                    .extract().as(McpSchema.JSONRPCResponse.class);
+
+            assertThat(result.error().message()).contains("Session ID required in Mcp-Session-Id header");
+        }
+
+        @Test
+        public void emptyBody_shouldThrowError() {
+            var result = given().header("Accept", "text/event-stream, application/json")
+                    .when()
+                    .post("/mcp")
+                    .then()
+                    .assertThat()
+                    .statusCode(StatusCode.BAD_REQUEST_CODE)
+                    .extract().as(McpSchema.JSONRPCResponse.class);
+
+            assertThat(result.error().message()).contains("Request body is missing");
+        }
+    }
+}
